842cbd5ca243d9fe659055ec5484808824804aee
Thomas P. Ogden <t@ogden.eu>
2020-12-17 16:21:41 +0000

    Solve 17a

--- 17a.py
+++ 17a.py
@@ -1,13 +1,13 @@
 import numpy as np
 
+np.set_printoptions(threshold=100000)
+
 def read_arr(fpath='data/17-demo.txt'):
     with open(fpath) as f:
         arr = [list(l.strip().replace('.', '0').replace('#', '1')) 
             for l in f.readlines()]
         arr = [[int(i) for i in a] for a in arr]
     return arr
-
-arr = read_arr(fpath='data/17-demo.txt')
 
 def init_cubes(arr, num_cycles=1):
 
@@ -20,9 +20,9 @@ def init_cubes(arr, num_cycles=1):
 
     print('start_dim:', start_dim, 'num_cycles:', num_cycles, 'dim:', dim)
 
-    cubes = np.zeros(shape=(num_cycles+1, dim, dim, dim))
-    cubes[0, 0, idx_0:idx_0+start_dim, idx_0:idx_0+start_dim] = start
-    print(cubes)
+    cubes = np.zeros(shape=(num_cycles+1, dim, dim, dim), dtype=np.int)
+    cubes[0, dim//2, idx_0:idx_0+start_dim, idx_0:idx_0+start_dim] = start
+    # print(cubes)
     return cubes
 
 def sum_neighbors(cubes, cycle=0, idx=(0,0,0)):
@@ -51,14 +51,52 @@ def sum_neighbors(cubes, cycle=0, idx=(0,0,0)):
     else:
         c_z_max = cubes.shape[3]
 
-    # print(idx)
-    # region = cubes[0]
     region = cubes[cycle, c_x_min:c_x_max, c_y_min:c_y_max, c_z_min:c_z_max]
-    print(region)
+    # print(region)
+    # print(region.shape)
     return np.sum(region) - cubes[cycle, idx[0], idx[1], idx[2]]
 
-cubes = init_cubes(arr, num_cycles=1)
+def sum_neighbors_all(cubes, cycle=0):
+    sna = np.zeros(cubes[cycle].shape)
+    for x in range(cubes[cycle].shape[0]):
+        for y in range(cubes[cycle].shape[1]):
+            for z in range(cubes[cycle].shape[0]):
+                sna[x, y, z] = sum_neighbors(cubes, cycle, idx=(x,y,z))
+    return sna
 
-sum_n = sum_neighbors(cubes, cycle=0, idx=(0,1,1))
+def next_is_active(cubes, cycle=0):
+    sna = sum_neighbors_all(cubes, cycle)
+    # print('sna:\n', sna)
+    rule_1 = np.logical_and(cubes[cycle], 
+        np.logical_or(sna == 2, sna == 3)).astype(int)
+    # print('rule_1:\n', rule_1)
+    rule_2 = np.logical_and(np.logical_not(cubes[cycle]), sna == 3).astype(int)
+    # print('rule_2:\n', rule_2)
+    # at max, one of rule_1 and rule_2 can be true, so we can sum them
+    return rule_1 + rule_2
 
-print(sum_n)
+def step_cubes(cubes, cycle=0):
+    sna = sum_neighbors_all(cubes, cycle=cycle)
+    cubes[cycle+1] = next_is_active(cubes, cycle=cycle)
+    return cubes
+
+def get_num_active(cubes):
+    return np.sum(cubes, axis=(1,2,3))
+
+def walk_cubes(cubes, num_cycles):
+    for cycle in range(num_cycles):
+        step_cubes(cubes, cycle=cycle)
+    return cubes
+
+# arr = read_arr(fpath='data/17-demo.txt')
+arr = read_arr(fpath='data/17.txt')
+
+NUM_CYCLES = 6
+
+cubes = init_cubes(arr, num_cycles=NUM_CYCLES)
+walk_cubes(cubes=cubes, num_cycles=NUM_CYCLES)
+
+num_active = get_num_active(cubes)
+print('num_active:', num_active)
+
+print('sol:', num_active[-1])
